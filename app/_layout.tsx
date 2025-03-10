11515681785aee1e07287d74391b2dc485661353
Hadddock <jhadddock@protonmail.com>
2025-03-09 19:19:21 -0700

    Get and display locale information

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,18 @@
 import { Stack } from 'expo-router';
+import React, { useState } from 'react';
+import { Text } from 'react-native';
+import * as Localization from 'expo-localization';
 
 export default function RootLayout() {
+  let [locale, setLocale] = useState(Localization.getLocales()[0].languageTag);
+
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <Text>Current Locale: {locale}</Text>
+    </>
   );
 }
