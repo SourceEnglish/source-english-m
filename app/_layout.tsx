67b7ef52fc0f99c681c0abcb925fa8f5bd49ffaf
Hadddock <jhadddock@protonmail.com>
2025-03-10 15:50:47 -0700

    Move language setting functionality to LanguageSelector component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,44 +10,6 @@ import LanguageSelector from '@/components/LanguageSelector';
 export default function RootLayout() {
   let [locale, setLocale] = useState(Localization.getLocales()[0].languageTag);
 
-  useEffect(() => {
-    const loadLanguage = async () => {
-      try {
-        let savedLanguage;
-        if (Platform.OS === 'web') {
-          savedLanguage = localStorage.getItem('language');
-        } else {
-          savedLanguage = await AsyncStorage.getItem('language');
-        }
-
-        if (savedLanguage) {
-          setLocale(savedLanguage);
-          await i18n.changeLanguage(savedLanguage);
-        } else {
-          await changeLanguage(locale);
-        }
-      } catch (error) {
-        console.error('Failed to load language', error);
-      }
-    };
-
-    loadLanguage();
-  }, []);
-
-  const changeLanguage = async (lng: string) => {
-    try {
-      await i18n.changeLanguage(lng);
-      setLocale(lng);
-      if (Platform.OS === 'web') {
-        localStorage.setItem('language', lng);
-      } else {
-        await AsyncStorage.setItem('language', lng);
-      }
-    } catch (error) {
-      console.error('Failed to change language', error);
-    }
-  };
-
   return (
     <>
       <I18nextProvider i18n={i18n}>
@@ -55,8 +17,8 @@ export default function RootLayout() {
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
-        <Text>Current Locale: {locale}</Text>
-        <LanguageSelector changeLanguage={changeLanguage} />{' '}
+
+        <LanguageSelector />
       </I18nextProvider>
     </>
   );
--- components/LanguageSelector.tsx
+++ components/LanguageSelector.tsx
@@ -1,45 +1,86 @@
 import React, { useState, useEffect } from 'react';
-import { Platform } from 'react-native';
+import { Platform, Text } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 import languages from '@/languages.json';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Localization from 'expo-localization';
+import i18n from 'i18next';
 
 interface LanguageSelectorProps {
-  changeLanguage: (lng: string) => void;
+  // changeLanguage: (lng: string) => void;
 }
 
-const LanguageSelector: React.FC<LanguageSelectorProps> = ({
-  changeLanguage,
-}) => {
+const LanguageSelector: React.FC<LanguageSelectorProps> = ({}) => {
+  let [locale, setLocale] = useState(Localization.getLocales()[0].languageTag);
+
+  useEffect(() => {
+    const loadLanguage = async () => {
+      try {
+        let savedLanguage;
+        if (Platform.OS === 'web') {
+          savedLanguage = localStorage.getItem('language');
+        } else {
+          savedLanguage = await AsyncStorage.getItem('language');
+        }
+
+        if (savedLanguage) {
+          setLocale(savedLanguage);
+          setValue(savedLanguage); // Set the current value in the language selector
+          await i18n.changeLanguage(savedLanguage);
+        } else {
+          await changeLanguage(locale);
+        }
+      } catch (error) {
+        console.error('Failed to load language', error);
+      }
+    };
+
+    loadLanguage();
+  }, []);
+
+  const changeLanguage = async (lng: string) => {
+    try {
+      await i18n.changeLanguage(lng);
+      setLocale(lng);
+      if (Platform.OS === 'web') {
+        localStorage.setItem('language', lng);
+      } else {
+        await AsyncStorage.setItem('language', lng);
+      }
+    } catch (error) {
+      console.error('Failed to change language', error);
+    }
+  };
+
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState<string | null>(null);
   const [items, setItems] = useState(languages);
 
-  useEffect(() => {
-    const fetchLanguage = async () => {
-      let language;
-      if (Platform.OS === 'web') {
-        language = localStorage.getItem('language');
-      } else {
-        language = await AsyncStorage.getItem('language');
-      }
-      if (language) {
-        setValue(language);
-        changeLanguage(language);
-      } else {
-        let defaultValue = Localization.getLocales()[0].languageTag;
-        if (defaultValue) {
-          setValue(defaultValue);
-        }
-      }
-    };
+  // useEffect(() => {
+  //   const fetchLanguage = async () => {
+  //     let language;
+  //     if (Platform.OS === 'web') {
+  //       language = localStorage.getItem('language');
+  //     } else {
+  //       language = await AsyncStorage.getItem('language');
+  //     }
+  //     if (language) {
+  //       setValue(language);
+  //       changeLanguage(language);
+  //     } else {
+  //       let defaultValue = Localization.getLocales()[0].languageTag;
+  //       if (defaultValue) {
+  //         setValue(defaultValue);
+  //       }
+  //     }
+  //   };
 
-    fetchLanguage();
-  }, [changeLanguage]);
+  //   fetchLanguage();
+  // }, [changeLanguage]);
 
   return (
     <>
+      <Text>Current Locale: {locale}</Text>
       <DropDownPicker
         open={open}
         value={value}
