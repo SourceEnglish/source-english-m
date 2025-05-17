f878bbc6050bf83aea5d811302e9e1a83becfd21
Justin Haddock <jhadddock@protonmail.com>
2025-05-16 18:57:59 -0700

    Add RiceIcon to lessonIconMap for improved icon representation

--- utils/iconMap.ts
+++ utils/iconMap.ts
@@ -1,10 +1,13 @@
 import AppleIcon from '@/assets/icons/licensed/apple.svg';
+import RiceIcon from '@/assets/icons/licensed/rice.svg';
 
 // ...other imports
 
 export const lessonIconMap: Record<string, React.FC<any>> = {
   food: AppleIcon,
   sood: AppleIcon,
+  apple: AppleIcon,
+  rice: RiceIcon,
   broods: AppleIcon,
   // ...etc
 };
