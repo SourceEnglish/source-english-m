ce6429ef8faef0bbc9492607885134ee854537b5
Justin Haddock <jhadddock@protonmail.com>
2025-05-30 15:18:26 -0700

    Add header styling

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -3,15 +3,24 @@ import { View, StyleSheet } from 'react-native';
 import ReadableText from '../ReadableText';
 
 const Header = ({ text, styling }: { text: string; styling?: string[] }) => (
-  <View style={styles.container}>
-    <ReadableText text={text} style={{ fontSize: 28, fontWeight: 'bold' }} />
-  </View>
+  <>
+    <View style={styles.container}>
+      <ReadableText text={text} style={{ fontSize: 28, fontWeight: 'bold' }} />
+    </View>
+    <View style={styles.hr} />
+  </>
 );
 
 const styles = StyleSheet.create({
   container: {
-    marginVertical: 12,
+    marginVertical: 6,
     alignItems: 'flex-start',
+  },
+  hr: {
+    width: '100%',
+    height: 1,
+    backgroundColor: '#bbb',
+    marginBottom: 12,
   },
 });
 
