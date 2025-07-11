e117d0b422127fda5355868878292655be9a95ce
Justin Haddock <jhadddock@protonmail.com>
2025-07-10 23:06:19 -0700

    Refactor LessonBacklink component to improve conditional rendering of lessons and tags

--- app/vocab/[entry].tsx
+++ app/vocab/[entry].tsx
@@ -526,11 +526,12 @@ export default function VocabEntryPage() {
       {/* Lesson backlink as last section: always show, pass tags if __lessons not present */}
       <LessonBacklink
         lessons={
-          Array.isArray((vocabEntry as any)['__lessons']) && (vocabEntry as any)['__lessons'].length > 0
+          Array.isArray((vocabEntry as any)['__lessons']) &&
+          (vocabEntry as any)['__lessons'].length > 0
             ? (vocabEntry as any)['__lessons']
             : Array.isArray((vocabEntry as any)['__tags'])
-              ? (vocabEntry as any)['__tags']
-              : []
+            ? (vocabEntry as any)['__tags']
+            : []
         }
       />
     </ScrollView>
